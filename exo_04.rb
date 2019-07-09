82a4bd3945dd322405ede2fc16e8476d339b495d
SengSerany <cranyseng@gmail.com>
2019-07-09 14:17:52 +0200

    ajout exo_04

---
+++ exo_04.rb
@@ -0,0 +1,10 @@
+puts "Salut, ça farte?
+
+# Le message affiché est:
+	exo_04.rb:1: unterminated string meets end of file
+	puts "Salut, ça farte?
+
+# La première ligne affiché est un message d'erreur, il dit que:
+# Dans la première ligne (:1:) il y a une erreur,
+# et que le "string" (suite de caractères) n'est pas correctement terminé
+# Il met enfin sur la seconde ligne le contenu du progamme.
