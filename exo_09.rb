6e01e0f5cb3e6f777557267b3e6866e7329b8a35
SengSerany <cranyseng@gmail.com>
2019-07-09 15:32:42 +0200

    Ajout exo_09

---
+++ exo_09.rb
@@ -0,0 +1,7 @@
+puts "Hey! salut toi! c'est quoi ton prénom?"
+print "> "
+user_firstname = gets.chomp
+puts " et ton nom?"
+print "> "
+user_lastname =gets.chomp
+puts "Bonjour, #{user_firstname} #{user_lastname}"
