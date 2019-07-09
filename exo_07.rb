9fe450af7d65a7c090d56624c02f659940ec7dad
SengSerany <cranyseng@gmail.com>
2019-07-09 15:21:35 +0200

    ajout exo_07

---
+++ exo_07.rb
@@ -0,0 +1,5 @@
+puts "Bonjour, c'est quoi ton blase ?"
+user_name = gets.chomp
+puts user_name
+
+# gets.chomp permet de recuillir une ligne de caractère (string) sans avoir le "\n" systématiquement accollé au string (signifiant un retour à la ligne) qu'à saisie la personne.
---
+++ exo_07_c.rb
@@ -0,0 +1,7 @@
+user_name = gets.chomp
+puts user_name
+
+# La différence entre les trois programme sont que:
+# 	=> le premier et le deuxieme contiennent un texte demandant a l'utilisateur d'entrer son pseudo.
+# 	=> Le dernier crée directement l'action de saisie
+# 	=> Le deuxième rajoute le caractère ">" devant la zone où l'on saisie.
