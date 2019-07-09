0fd29a820b3cf612cef4dbc90d5d2cc2866d9c39
SengSerany <cranyseng@gmail.com>
2019-07-09 14:10:44 +0200

    ajout exo_03

---
+++ exo_03.rb
@@ -0,0 +1,5 @@
+puts "Bonjour, monde !"
+#Et avec une voix sexy, ça donne : Bonjour, monde !
+
+# avec un "#" devant ça indique l'écriture d'un commentaire 
+# et donc ça ne s'affiche pas quand on execute le dossier sur le Terminal
