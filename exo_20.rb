04818deb890ee536052c8f9b8861f1b817ab6b30
SengSerany <52448207+SengSerany@users.noreply.github.com>
2019-07-10 10:23:35 +0200

    Ajout exo_20

--- exo_20.rb
+++ exo_20.rb
@@ -1,11 +1,11 @@
 puts " Choisis un nombre entre 1 et 25:"
 print "> "
 etages = gets.chomp.to_i
-If etages >=1 && etages <=25
+if etages >=1 && etages <=25
 etages.times do |etage|
 	pierre = ("#"*etage)
 	puts pierre + "#"
-Else
-Puts "Try again!" 
-end
+	end
+else
+puts "Try again!" 
 end
